8aa80093289e1e8798b5261551cf5038ba8132f3
Nasir <momin96@gmail.com>
2018-04-03 13:02:06 +0530

    Initial app setup

---
+++ NSRDemoAppJSON/NSRDemoAppJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSRDemoAppJSON
+//
+//  Created by Nasir Ahmed Momin on 03/04/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
